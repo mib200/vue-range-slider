21cc32018e5684680f0563e78660aafc7a8e625e
Manish Kumar <mib200@gmail.com>
2016-12-05 17:51:58 +0530

    browser check for SSR

--- src/DocumentEventHelper.js
+++ src/DocumentEventHelper.js
@@ -1,19 +1,23 @@
 // @flow
 /* global document */
-
+var inBrowser = typeof window !== 'undefined';
 export default {
-  created () {
-    forEachListener(this, (key, listener) => {
-      document.addEventListener(key, listener)
-    })
-  },
+	created() {
+		if (inBrowser) {
+			forEachListener(this, (key, listener) => {
+				document.addEventListener(key, listener)
+			});
+		}
+	},
 
-  beforeDestroy () {
-    forEachListener(this, (key, listener) => {
-      document.removeEventListener(key, listener)
-    })
-  }
-}
+	beforeDestroy() {
+		if (inBrowser) {
+			forEachListener(this, (key, listener) => {
+				document.removeEventListener(key, listener)
+			});
+		}
+	}
+};
 
 function forEachListener(vm: any, f: (key: string, listener: Function) => void) {
   const events = vm.$options.events
